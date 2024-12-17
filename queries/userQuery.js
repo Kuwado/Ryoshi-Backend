7c14054d168fbecb55004aeded985e7056e880c2
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-12-17 20:49:59 +0700

    edit api get list users

--- queries/userQuery.js
+++ queries/userQuery.js
@@ -1,6 +1,33 @@
 const { where } = require("sequelize");
 const db = require("../models/index");
 
+const getAllUsers = async () => {
+  const users = await db.User.findAll({
+    attributes: {
+      exclude: ["password"],
+    },
+    include: [
+      {
+        model: db.location,
+        as: "liked_location",
+        through: {
+          model: db.Liked,
+          attributes: ["id"],
+        },
+      },
+      {
+        model: db.location,
+        as: "gone_location",
+        through: {
+          model: db.Gone,
+          attributes: ["id"],
+        },
+      },
+    ],
+  });
+  return users;
+};
+
 const getUser = async (id) => {
   const user = await db.User.findByPk(id, {
     attributes: {
@@ -65,4 +92,11 @@ const forgotPass = async (email, newPass) => {
   return count;
 };
 
-module.exports = { updateUser, getUser, updateAva, updatePass, forgotPass };
+module.exports = {
+  updateUser,
+  getUser,
+  updateAva,
+  updatePass,
+  forgotPass,
+  getAllUsers,
+};
